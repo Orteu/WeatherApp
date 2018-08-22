26a11fdaf1bbfd77a943eb65adbe26bed6a76bb3
Carlos Saiz <carlos.saiz@etoolinnovation.com>
2018-08-22 09:20:06 +0200

    Using fetch to get info from API Rest

--- src/components/WeatherComponent/index.js
+++ src/components/WeatherComponent/index.js
@@ -3,18 +3,15 @@ import Location from './Location';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import './style.css';
 
+const location = "Palma de Mallorca,es";
+const api_key = "44fa2ff95e24fb821c0288b95d45a743";
+const api_url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 20,
     humidity: 10,
     wind: 10,
     weatherState: 'cloudy'
-};
-
-const data2 = {
-    temperature: 30,
-    humidity: 20,
-    wind: 5,
-    weatherState: 'sun'
 };
 
 class WeatherComponent extends React.Component {
@@ -28,9 +25,10 @@ class WeatherComponent extends React.Component {
     }
 
     refreshWeather = () => {
-        this.setState({
-            data: data2
-        });
+        fetch(api_url);
+        // this.setState({
+        //     data: data2
+        // });
         console.log("Weather updated");
     };
 
@@ -39,7 +37,7 @@ class WeatherComponent extends React.Component {
         return(
             <div className="container">
                 <Location city={city}/>
-                <WeatherExtraInfo data={data}/>
+                <WeatherExtraInfo data={data}/><br/>
                 <button onClick={this.refreshWeather}>Refresh</button>
             </div>
         );
