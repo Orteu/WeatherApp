46c7688255a75e109ad16fe345d00a6c81e5817d
Carlos Saiz <orteu15@gmail.com>
2018-08-23 12:34:43 +0200

    Remove unnecessaries components

--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Navbar = () => (
-    <div>Navbar goes here</div>
-);
-
-export default Navbar;
--- src/components/Slider.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Slider = () => (
-    <div>Slider with images comes here</div>
-);
-
-export default Slider;
