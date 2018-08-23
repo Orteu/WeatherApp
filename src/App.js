bf994d04b9c1136773106727aedfe2eff6277919
Carlos Saiz <orteu15@gmail.com>
2018-08-23 09:18:00 +0200

    AppBar, React-flexBox-Grid and others integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import './App.css';
 import LocationList from "./components/LocationList";
 
+import * as Colors from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// Cities Array while we can't get them from user
 const cities = ["Palma de Mallorca", "Manchester", "New York", "Zagreb"];
+
+// AppBar Settings
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: Colors.darkBlack,
+        alternateTextColor: Colors.white
+    }
+});
 
 class App extends Component {
     handleSelectedLocation = city => {
@@ -12,14 +26,25 @@ class App extends Component {
     };
     render() {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={muiTheme}>
                 <Grid>
                     <Row>
+                        <Col xs={12}>
+                            <AppBar title="My Weather App" color={"black"} />
+                        </Col>
+                    </Row>
+                    <Row>
                         <Col xs={12} md={6}>
-                            <LocationList cities = {cities} onSelectedLocation={this.handleSelectedLocation}/>
+                            <Paper zDepth={2}>
+                                <div className="Locations">
+                                    <LocationList cities = {cities} onSelectedLocation={this.handleSelectedLocation}/>
+                                </div>
+                            </Paper>
                         </Col>
                         <Col  xs={12} md={6}>
-                            <div className="weatherDetail"></div>
+                            <Paper zDepth={2}>
+                                <div className="weatherDetail"></div>
+                            </Paper>
                         </Col>
                     </Row>
                 </Grid>
