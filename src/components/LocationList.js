6229c2b4993b943df66143183836a0b191a68a84
Carlos Saiz <orteu15@gmail.com>
2018-08-23 09:37:34 +0200

    Scroll on Locations when the screen is small

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherComponent from "./WeatherComponent";
+import './style.css';
 
 const LocationList = ({ cities, onSelectedLocation }) => {
     const handleWeatherLocationClick = city => {
@@ -18,7 +19,7 @@ const LocationList = ({ cities, onSelectedLocation }) => {
     );
 
     return(
-        <div>
+        <div className="locationList">
             {strToComponent(cities)}
         </div>
     );
