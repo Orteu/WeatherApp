c9bc69e471dccf77d9559d3f60c8e79ad39e0301
Carlos Saiz <orteu15@gmail.com>
2018-09-05 14:40:35 +0200

    Fix bug on select location

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -6,18 +6,20 @@ export const cities = (state = {}, action) => {
     switch (action.type) {
         case SET_FORECAST_DATA: {
             const { city, forecastData } = action.payload;
-            return {...state, [city]: { forecastData }};
+            return {
+                ...state, [city]: { ...state[city], forecastData}
+            }
         }
         case GET_WEATHER_CITY: {
             const city = action.payload;
             return {
-                ...state, [city]: {weather: null}
+                ...state, [city]: {...state[city], weather: null}
             }
         }
         case SET_WEATHER_CITY: {
             const { city, weather } = action.payload;
             return {
-                ...state, [city]: {weather}
+                ...state, [city]: {...state[city],weather}
             }
         }
         default:
